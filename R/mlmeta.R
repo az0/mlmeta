eed9894acd7a649e819330d290fc1766e0b2a9b9
Andrew Ziem <ahz001@gmail.com>
2014-11-07 10:48:55 -0700

    Expand documentation

---
+++ R/mlmeta.R
@@ -0,0 +1,6 @@
+#` Machine Learning Programming for R
+#`
+#` Export machine learning models trained in R to other programming
+#` languages, so the models can score data in a separate environment
+#` without R.
+NULL
--- R/nnet2sas.R
+++ R/nnet2sas.R
@@ -16,7 +16,10 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-
+#` Export an artificial neural network to SAS
+#`
+#` @param fit a neural network fitted by nnet()
+#` @export
 nnet2sas <- function(fit)
 {
     if(!inherits(fit, c('train','nnet')))
