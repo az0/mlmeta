090777dc13e6de0115c5d5721436cfda313d1c0e
Andrew Ziem <ahz001@gmail.com>
2014-12-01 22:28:59 -0700

    Add test for Bernoulli

--- tests/testthat/test_gbm2sas.R
+++ tests/testthat/test_gbm2sas.R
@@ -1,8 +1,21 @@
 test_that('gbm2sas returns a character',  {
 
     require(gbm)
+
+    # Gaussian distribution
     data(mtcars)
-    gbm.fit <- gbm(mpg ~ cyl + disp, data=mtcars, bag.fraction=1.1,distribution="gaussian")
-    ret <- gbm2sas(gbm.fit)
+    gbm.fit.g <- gbm(mpg ~ cyl + disp, data = mtcars, bag.fraction = 1.1,distribution = "gaussian")
+    ret <- gbm2sas(gbm.fit.g)
     expect_that(ret, is_a('character'))
+
+    # Bernoulli distribution
+    data(iris)
+    iris2 <- iris[iris$Species %in% c('setosa', 'versicolor'),]
+    iris2$Species <- factor(iris2$Species)
+    gbm.fit.b <- gbm(Species ~ ., data = iris2, bag.fraction = 1.1, distribution = "gaussian")
+    ret <- gbm2sas(gbm.fit.b)
+    expect_that(ret, is_a('character'))
+
+
+
 })
