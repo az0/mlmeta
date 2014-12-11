cce2b4e283716a35d3a587ffe2a6b3df08686c5c
Andrew Ziem <aziem@us.ci.org>
2014-12-11 15:46:45 -0700

    Fix 742b37570da78b32e292d583dc1caa9136bd625a

--- R/ctree2sas.R
+++ R/ctree2sas.R
@@ -85,8 +85,8 @@ ctree2sas <- function(mytree, node_id = 1, parent_criteria = character(0))
 {
     require(party)
     if (party::nodes(mytree, node_id)[[1]]$terminal) {
-        prediction <- btree_prediction(mytree, node_id)
-        ret <- paste('else if', parent_criteria, 'then prediction =',prediction,'; /* node',node_id,'*/')
+        ret <- btree_prediction(mytree, node_id)
+        ret <- paste('else if', parent_criteria, 'then prediction =',ret,'; /* node',node_id,'*/')
         return (ret)
     }
 
